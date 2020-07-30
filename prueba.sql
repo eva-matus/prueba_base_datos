28e70962d294288030e1c215c548c50122ecadf9
Eva <eva.matus@hotmail.com>
2020-07-29 20:40:56 -0400

    sql

---
+++ prueba.sql
@@ -0,0 +1,74 @@
+eva=> CREATE DATABASE prueba;
+CREATE DATABASE
+eva=> \c prueba;
+You are now connected to database "prueba" as user "eva".
+
+prueba=> CREATE TABLE cliente(
+prueba(> id_cliente SERIAL,
+prueba(> rut VARCHAR,
+prueba(> nombre VARCHAR,
+prueba(> direccion VARCHAR,
+prueba(> PRIMARY KEY (id_cliente));
+CREATE TABLE
+prueba=> SELECT * FROM cliente;
+ id_cliente | rut | nombre | direccion 
+------------+-----+--------+-----------
+(0 rows)
+
+prueba=> CREATE TABLE factura(
+prueba(> numero_factura INT,
+prueba(> id_cliente INT,
+prueba(> fecha_factura DATE,
+prueba(> codigo_producto INT,
+prueba(> descripcion_producto VARCHAR,
+prueba(> cantidad_producto INT,
+prueba(> valor_unitario INT,
+prueba(> sub_total INT,
+prueba(> iva FLOAT,
+prueba(> precio_total INT,
+prueba(> FOREIGN KEY(numero_factura) REFERENCES cliente(id_cliente),
+prueba(> PRIMARY KEY(id_cliente));
+CREATE TABLE
+
+numero_factura | id_cliente | fecha_factura | codigo_producto | descripcion_producto | cantidad_producto | valor_unitario | sub_total | iva | precio_total 
+----------------+------------+---------------+-----------------+----------------------+-------------------+----------------+-----------+-----+--------------
+(0 rows)
+
+
+prueba=> CREATE TABLE producto(
+prueba(> codigo_producto INT,
+prueba(> descripcion_producto VARCHAR,
+prueba(> valor_unitario INT,
+prueba(> PRIMARY KEY (codigo_producto));
+CREATE TABLE
+
+prueba=> SELECT * FROM producto;
+ codigo_producto | descripcion_producto | valor_unitario 
+-----------------+----------------------+----------------
+(0 rows)
+
+
+prueba=> CREATE TABLE categoria(
+prueba(> id_producto INT,
+prueba(> nombre_categoria VARCHAR,
+prueba(> id_categoria INT,
+prueba(> FOREIGN KEY(id_producto) REFERENCES productoe(codigo_producto),
+prueba(> FOREIGN KEY(id_producto) REFERENCES producto(codigo_producto),
+prueba(> PRIMARY KEY(codigo_producto));
+ERROR:  column "codigo_producto" named in key does not exist
+LINE 7: PRIMARY KEY(codigo_producto));
+        ^
+prueba=> CREATE TABLE categoria(
+prueba(> codigo_producto INT,
+prueba(> nombre_categoria VARCHAR,
+prueba(> id_categoria INT,
+prueba(> FOREIGN KEY(codigo_producto) REFERENCES producto(codigo_producto),
+prueba(> PRIMARY KEY(codigo_producto));
+CREATE TABLE
+
+prueba=> SELECT * FROM categoria;
+ codigo_producto | nombre_categoria | id_categoria 
+-----------------+------------------+--------------
+(0 rows)
+
+
