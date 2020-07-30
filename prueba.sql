c7b19a2521dbfc322236a306023c63a7ad213801
Eva <eva.matus@hotmail.com>
2020-07-30 14:49:22 -0400

    data add

--- prueba.sql
+++ prueba.sql
@@ -7,6 +7,18 @@ finalmente realizar consultas sobre la misma. Para realizar los diagramas se rec
 draw.io
 
 
+Parte 1: Crear modelo lógico para el siguiente caso.
+Una Factura pertenece a un cliente
+El cliente tiene id, nombre, rut y dirección
+Una factura tiene numero de factura, fecha de la factura, subtotal, iva y precio total
+Una factura tiene un listado de productos, con su precio unitario, cantidad, y valor total por
+producto.
+Para la factura se deberá almacenar el subtotal de la factura, que corresponde a la suma de
+todos los valores unitarios multiplicado por la cantidad respectiva.
+Cada producto tiene un id, un nombre, descripción y un valor unitario
+Cada producto pertenece a una categoría, la cual tiene un id, el nombre de la categoría y una
+descripción.
+
 
 eva=> CREATE DATABASE prueba;
 CREATE DATABASE
@@ -87,5 +99,18 @@ prueba=> SELECT * FROM categoria;
  codigo_producto | nombre_categoria | id_categoria 
 -----------------+------------------+--------------
 (0 rows)
+
+Parte 2: Creando el modelo en la base de datos
+Se debe crear el modelo en la base de datos, en una base de datos llamada prueba , e insertar los
+siguientes registros:
+5 clientes
+8 productos
+3 categorias
+10 facturas
+2 para el cliente 1, con 2 y 3 productos
+3 para el cliente 2, con 3, 2 y 3 productos
+1 para el cliente 3, con 1 producto
+4 para el cliente 4, con 2, 3, 4 y 1 producto.
+
 
 
